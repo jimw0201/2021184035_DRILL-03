7313f3b4befaec1a28ea2a349571fdc1e4891d6b
minwooji <jimw0201@gmail.com>
2024-09-24 10:04:01 +0900

    코드 전체 기반 작성

---
+++ character_grass.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+open_canvas()
+
+# fill here
+grass = load_image("grass.png")
+boy = load_image("character.png")
+
+def run_rectangle():
+    pass
+
+def run_circle():
+    pass
+
+while True:
+    run_rectangle()
+    run_circle()
+
+close_canvas()
