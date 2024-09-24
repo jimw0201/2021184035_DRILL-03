6e3645fcf310c3f5041f10c69050a61df0a91fe8
minwooji <jimw0201@gmail.com>
2024-09-24 10:10:14 +0900

    boy를 캔버스 중앙에 출력해봄

--- character_grass.py
+++ character_grass.py
@@ -12,10 +12,15 @@ def run_rectangle():
 
 def run_circle():
     print('CIRCLE')
+    
+    clear_canvas_now()
+    boy.draw_now(400, 300)
+    delay(1)
     pass
 
 while True:
     run_rectangle()
     run_circle()
+    break
 
 close_canvas()
